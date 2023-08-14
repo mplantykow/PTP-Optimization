a08a05b95ed35aadc47d2711b0f3e541a115c600
mplantykow <m.plantykow@gmail.com>
2023-08-14 22:51:28 +0200

    Lint code
    Signed-off-by: Marta Plantykow <m.plantykow@gmail.com>

--- evaluate.py
+++ evaluate.py
@@ -5,13 +5,13 @@
 # You may obtain a copy of the License at
 #     https://spdx.org/licenses/GPL-2.0-or-later.html
 
-import random
-import numpy
+"""GA for PID in PTP."""
+
 import subprocess #nosec
 from shlex import split
+import sys
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-import sys
 import configureme as config
 
 Rating_table = []
@@ -19,33 +19,39 @@ Checked_data = []
 Master_offset = []
 
 class Creature():
+    """Creature class."""
     rating = 0
 
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
+    def __init__(self, k_p, k_i):
+        """Init function."""
+        self.k_p = k_p
+        self.k_i = k_i
         rating = 0
 
-    def mutate(self, new_Kp, new_Ki):
-        self.Kp = new_Kp
-        self.Ki = new_Ki
+    def mutate(self, new_k_p, new_k_i):
+        """Function mutating data."""
+        self.k_p = new_k_p
+        self.k_i = new_k_i
 
-    #Evaluate data using one of three metrics - MSE, RMSE, MAE
-    def evaluate_data(self, interface, t):
-
-        #Check if provided Kp and Ki are not repeated
+    def evaluate_data(self, interface, time):
+        """Function evaluationg data."""
+        #Check if provided k_p and k_i are not repeated
         repeated_data = self.validate_data()
-        if(repeated_data == 0):
+        if repeated_data == 0:
             print("Correct data!")
             try:
-                if(config.app == "phc2sys"):
-                    subprocess.check_call(split('./test-phc2sys.sh -s %s -c CLOCK_REALTIME -P %s -I %s -t %s' % (str(interface), str(self.Kp), str(self.Ki), str(t))))
-                elif (config.app == "ptp4l"):
-                    subprocess.check_call(split('./test-ptp4l.sh -i %s -P %s -I %s -t %s' % (str(interface), str(self.Kp), str(self.Ki), str(t))))
+                if config.app == "phc2sys":
+                    subprocess.check_call(
+                            split('./test-phc2sys.sh -s %s -c CLOCK_REALTIME -P %s -I %s -t %s' %
+                                 (str(interface), str(self.k_p), str(self.k_i), str(time))))
+                elif config.app == "ptp4l":
+                    subprocess.check_call(
+                            split('./test-ptp4l.sh -i %s -P %s -I %s -t %s' %
+                                 (str(interface), str(self.k_p), str(self.k_i), str(time))))
             except subprocess.SubprocessError:
-                if(config.app == "phc2sys"):
+                if config.app == "phc2sys":
                     print("Error calling phc2sys")
-                elif (config.app == "ptp4l"):
+                elif config.app == "ptp4l":
                     print("Error calling ptp4l")
                 sys.exit()
             self.get_data_from_file()
@@ -55,97 +61,105 @@ class Creature():
                 for i in range(len(Master_offset)):
                     print(Master_offset[i])
 
-            stripped_Master_offset = Master_offset[2::]
+            stripped_master_offset = Master_offset[2::]
 
             if config.debug_level != 1:
-                for i in range(len(stripped_Master_offset)):
-                    print(stripped_Master_offset[i])
+                for i in range(len(stripped_master_offset)):
+                    print(stripped_master_offset[i])
 
             #Calculate MSE
-            if(config.metric=="MSE"):
+            if config.metric=="MSE":
                 if config.debug_level != 1:
                     print("Choosen metric: MSE")
-                rating = rate_data_MSE(stripped_Master_offset)
+                rating = rate_data_mse(stripped_master_offset)
             #Calculate RMSE
-            elif(config.metric=="RMSE"):
+            elif config.metric=="RMSE":
                 if config.debug_level != 1:
                     print("Choosen metric: RMSE")
-                rating = rate_data_RMSE(stripped_Master_offset)
+                rating = rate_data_rmse(stripped_master_offset)
             #Calculate MAE
-            elif(config.metric=="MAE"):
+            elif config.metric=="MAE":
                 if config.debug_level != 1:
                     print("Choosen metric: MAE")
-                rating = rate_data_MAE(stripped_Master_offset)
+                rating = rate_data_mae(stripped_master_offset)
 
             Rating_table.append(rating)
-        #If Kp and Ki are repeated, return previously calculated rating
+        #If k_p and k_i are repeated, return previously calculated rating
         else:
             print("Evaluate.py: Incorrect data!")
             rating = Rating_table[repeated_data - 1]
 
         self.rating = rating
 
-    #Validate Kp and Ki
     def validate_data(self):
-        if(len(Checked_data) > 0):
-            iter = 1
+        """Function validating data."""
+        if len(Checked_data) > 0:
+            cntr = 1
             for creature in Checked_data:
-                if(creature.Kp == self.Kp and creature.Ki == self.Ki):
-                    return iter
-                iter = iter + 1
+                if(creature.k_p == self.k_p and creature.k_i == self.k_i):
+                    return cntr
+                cntr = cntr + 1
 
-        Checked_data.append(Creature(self.Kp, self.Ki))
+        Checked_data.append(Creature(self.k_p, self.k_i))
         if config.debug_level != 1:
             print("Number of already checked data: ", len(Checked_data))
         return 0
 
-    #Get master offset from file
     def get_data_from_file(self):
+        """Function getting master offset from file."""
         Master_offset.clear()
-        if(config.app == "phc2sys"):
-            file_name = "phc2sys_P%s_I%s/phc2sys_P%s_I%s.log" % (str(self.Kp), str(self.Ki), str(self.Kp), str(self.Ki))
-        elif (config.app == "ptp4l"):
-            file_name = "ptp4l_P%s_I%s/ptp4l_P%s_I%s.log" % (str(self.Kp), str(self.Ki), str(self.Kp), str(self.Ki))
+        if config.app == "phc2sys":
+            file_name = "phc2sys_P%s_I%s/phc2sys_P%s_I%s.log" % (str(self.k_p),
+                                                                 str(self.k_i),
+                                                                 str(self.k_p),
+                                                                 str(self.k_i))
+        elif config.app == "ptp4l":
+            file_name = "ptp4l_P%s_I%s/ptp4l_P%s_I%s.log" % (str(self.k_p),
+                                                             str(self.k_i),
+                                                             str(self.k_p),
+                                                             str(self.k_i))
+        else:
+            file_name = "filename"
 
-        with open(file_name, 'r') as read_file:
+        with open(file_name, 'r', encoding="utf-8") as read_file:
             for line in read_file:
                 splitted = line.split()
-                if(config.app == "phc2sys"):
+                if config.app == "phc2sys":
                     Master_offset.append(splitted[4])
-                elif (config.app == "ptp4l"):
+                elif config.app == "ptp4l":
                     Master_offset.append(splitted[3])
 
         return Master_offset
 
-#Calculate MSE
-def rate_data_MSE(data):
+def rate_data_mse(data):
+    """Function calculationg MSE."""
     arr = [0 for i in range(len(data))]
 
     array1 = list(map(float, arr))
     array2 = list(map(int, data))
-    MSE = mean_squared_error(array1, array2)
-    print("MSE: ", MSE)
+    mse = mean_squared_error(array1, array2)
+    print("MSE: ", mse)
 
-    return MSE
+    return mse
 
-#Calculate RMSE
-def rate_data_RMSE(data):
+def rate_data_rmse(data):
+    """Function calculating RMSE."""
     arr = [0 for i in range(len(data))]
 
     array1 = list(map(float, arr))
     array2 = list(map(int, data))
-    RMSE = mean_squared_error(array1, array2, squared=False)
-    print("RMSE: ", RMSE)
+    rmse = mean_squared_error(array1, array2, squared=False)
+    print("RMSE: ", rmse)
 
-    return RMSE
+    return rmse
 
-#Calculate MAE
-def rate_data_MAE(data):
+def rate_data_mae(data):
+    """Function calculating MAE."""
     arr = [0 for i in range(len(data))]
 
     array1 = list(map(float, arr))
     array2 = list(map(int, data))
-    MAE = mean_absolute_error(array1, array2)
-    print("MAE: ", MAE)
+    mae = mean_absolute_error(array1, array2)
+    print("MAE: ", mae)
 
-    return MAE
+    return mae
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Module providing GA for PTP PI controller."""
 #!/usr/bin/python3
 # Copyright (c) 2021 Intel
 # Licensed under the GNU General Public License v2.0 or later (the "License");
@@ -7,15 +8,14 @@
 
 import sys
 import random
-import numpy
-import subprocess #nosec
-import shlex
-import argparse
 import os
+import argparse
+import numpy
 import configureme as config
 from evaluate import Creature
 
 class Range():
+    """Class providing range"""
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -26,78 +26,78 @@ class Range():
     def __iter__(self):
         yield self
 
-def validate_stability(Kp, Ki):
-    eq1 = (((Kp + Ki)*(Kp + Ki)) < (4*Ki))
-    eq2 = ((Ki > 0) and (Ki <= 4))
-    eq3 = ((Kp > 0) and (Kp <= 1))
+def validate_stability(k_p, k_i):
+    """Function validating stability."""
+    eq1 = (((k_p + k_i)*(k_p + k_i)) < (4*k_i))
+    eq2 = 0 <= k_i <= 4
+    eq3 = 0 <= k_p <= 1
     if eq1 and eq2 and eq3:
         return True
-    else:
-        return False
+    return False
 
 def draw_stable_kp_ki():
+    """Function drawing stable k_p and k_i pair."""
     stable = False
     while not stable:
-        Kp = random.uniform(0, config.gen_max_kp_stable)
-        Ki = random.uniform(0, config.gen_max_ki_stable)
-        if validate_stability(Kp, Ki):
+        k_p = random.uniform(0, config.gen_max_kp_stable)
+        k_i = random.uniform(0, config.gen_max_ki_stable)
+        if validate_stability(k_p, k_i):
             stable = True
-    return Kp, Ki
+    return k_p, k_i
 
-def redefine_kp_ki_to_stable(Kp, Ki):
-    if validate_stability(Kp, Ki):
-        return Kp, Ki
-    with open("stability.csv", "a") as f:
-        os.chmod("stability.csv", 0o600)
+def redefine_kp_ki_to_stable(k_p, k_i):
+    """Function redefining k_p and k_i to stable."""
+    if validate_stability(k_p, k_i):
+        return k_p,k_i
     stable = False
     while not stable:
-        with open("stability.csv", "a") as f:
-            f.write(f"{Ki};{Kp}\n")
-        if Ki < 1:
-            Ki = Ki + (1 - Ki) * config.reduction_determinant
-        if Ki > 1:
-            Ki = Ki - (Ki - 1) * config.reduction_determinant
-        if Ki == 0:
-            Ki = Ki + config.reduction_determinant
-        if Kp == 0:
-            Kp = Kp + config.reduction_determinant
-        Kp = Kp - config.reduction_determinant * Kp
-        Ki = round(Ki, 3)
-        Kp = round(Kp, 3)
-        if validate_stability(Kp, Ki):
+        with open("stability.csv", "a", encoding="utf-8") as stabilityfile:
+            stabilityfile.write(f"{k_i};{k_p}\n")
+        if k_i < 1:
+            k_i = k_i + (1 - k_i) * config.reduction_determinant
+        if k_i > 1:
+            k_i = k_i - (k_i - 1) * config.reduction_determinant
+        if k_i == 0:
+            k_i = k_i + config.reduction_determinant
+        if k_p == 0:
+            k_p = k_p + config.reduction_determinant
+        k_p = k_p - config.reduction_determinant * k_p
+        k_i = round(k_i, 3)
+        k_p = round(k_p, 3)
+        if validate_stability(k_p, k_i):
             stable = True
-    return Kp, Ki
+    return k_p,k_i
 
-if ((config.metric != "MSE") and (config.metric != "RMSE") and (config.metric != "MAE")):
+if config.metric not in {"MSE", "RMSE", "MAE"}:
     print("Specify one of the following metrics: MSE, RMSE, MAE")
-    exit()
-if (config.gen_population_size < 8):
+    sys.exit()
+if config.gen_population_size < 8:
     print("Min population size: 8")
-    exit()
-if (config.gen_epochs < 1):
+    sys.exit()
+if config.gen_epochs < 1:
     print("Min number of epochs: 1")
-    exit()
-if (config.gen_max_kp <= 0):
-    print("Max Kp must be greater than 0")
-    exit()
-if (config.gen_max_ki <= 0):
-    print("Max Ki must be greater than 0")
-    exit()
-if (config.gen_num_random < 0):
+    sys.exit()
+if config.gen_max_kp <= 0:
+    print("Max k_p must be greater than 0")
+    sys.exit()
+if config.gen_max_ki <= 0:
+    print("Max k_i must be greater than 0")
+    sys.exit()
+if config.gen_num_random < 0:
     print("The number of randomly added creatures must be greater or equal to 0")
-    exit()
-if (config.gen_num_inherited < 0):
+    sys.exit()
+if config.gen_num_inherited < 0:
     print("The number of inherited creatures must be greater or equal to 0")
-    exit()
-if (config.gen_num_replicated < 0):
+    sys.exit()
+if config.gen_num_replicated < 0:
     print("Number of replicated creatures must be greater or equal to 0")
-    exit()
-if (config.gen_mutation_coef < 1):
+    sys.exit()
+if config.gen_mutation_coef < 1:
     print("Mutation coef must be greater or equal 1")
-    exit()
-if (config.gen_elite_size < 0):
+    sys.exit()
+if config.gen_elite_size < 0:
     print("Elite size must be greater or equal 0")
-    exit()
+    sys.exit()
 
 #Validate interface
 adapterlist = os.listdir('/sys/class/net/')
@@ -105,11 +105,12 @@ parser = argparse.ArgumentParser(description='Genetic algorithm for PID in PTP i
 
 #List of arguments
 parser.add_argument("--i", type=str, choices = adapterlist, help="Interface")
-parser.add_argument("--t", default=120, choices=range(1,9999), type=int, help="-t from PTP script", metavar="[1-9999]")
+parser.add_argument("--t", default=120, choices=range(1,9999), type=int,
+                    help="-t from PTP script", metavar="[1-9999]")
 
 args = parser.parse_args()
 
-if (config.stability_verification == True):
+if config.stability_verification is True:
     print("Stability verification enabled")
 
 #Initial population
@@ -123,28 +124,29 @@ if (config.gen_mutation_coef > 1 or config.gen_mutation_coef < -1):
 
 for _ in range(config.gen_population_size):
     if (config.initial_values == True):
-        Kp = config.initial_kp
-        Ki = config_initial_ki
-    elif (config.stability_verification == True):
-        Kp,Ki = draw_stable_kp_ki()
-        population.append(Creature(Kp,Ki))
+        k_p = config.initial_kp
+        k_i = config_initial_ki
+    if config.stability_verification is True:
+        k_p,k_i = draw_stable_kp_ki()
+        population.append(Creature(k_p,k_i))
     else:
-        population.append(Creature(random.uniform(0, config.gen_max_kp), random.uniform(0, config.gen_max_ki))) #nosec
+        population.append(Creature(random.uniform(0, config.gen_max_kp),
+                                   random.uniform(0, config.gen_max_ki))) #nosec
 
-iter = 0
+cntr = 0
 print("Initial population created!")
 
-if (config.debug_level != 1):
+if config.debug_level != 1:
     for creature in population:
-        print("Creature", iter)
-        print(creature.Kp)
-        print(creature.Ki)
-        iter = iter + 1
+        print("Creature", cntr)
+        print(creature.k_p)
+        print(creature.k_i)
+        cntr = cntr + 1
 
-with open("ptp_optimization.log", "a") as f:
-        f.write(f"***************************************************************\n")
-        f.write(f"Genetic algorithm results:\n")
-        os.chmod("ptp_optimization.log", 0o600);
+with open("ptp_optimization.log", "a", encoding="utf-8") as f:
+    f.write("***************************************************************\n")
+    f.write("Genetic algorithm results:\n")
+    os.chmod("ptp_optimization.log", 0o600)
 
 for epoch in range(config.gen_epochs):
     print("***************************************************************")
@@ -158,11 +160,11 @@ for epoch in range(config.gen_epochs):
     i = 0
     for parent in population:
         print("Evaluating creature number ", i)
-        new_kp = round(parent.Kp,3)
-        new_ki = round(parent.Ki,3)
-        parent.mutate(new_kp, new_ki)
-        print("Kp: ", new_kp)
-        print("Ki: ", new_ki)
+        new_k_p = round(parent.k_p,3)
+        new_k_i = round(parent.k_i,3)
+        parent.mutate(new_k_p, new_k_i)
+        print("k_p: ", new_k_p)
+        print("k_i: ", new_k_i)
         parent.evaluate_data(args.i, args.t)
         score.append(parent.rating)
         i = i + 1
@@ -175,10 +177,10 @@ for epoch in range(config.gen_epochs):
     #Pick the best result and save it to the file
     index = sorted_scores_indexes[0]
 
-    with open("ptp_optimization.log", "a") as f:
+    with open("ptp_optimization.log", "a", encoding="utf-8") as f:
         f.write(f"Epoch number: {epoch}\n")
-        f.write(f"Kp: {population[index].Kp} ")
-        f.write(f"Ki: {population[index].Ki} ")
+        f.write(f"k_p: {population[index].k_p} ")
+        f.write(f"k_i: {population[index].k_p} ")
         f.write(f"Score: {score[index]}\n")
         #Uncomment to print all scores to the file
         #for i in range(0, len(score)):
@@ -196,7 +198,7 @@ for epoch in range(config.gen_epochs):
     elite.sort(key = lambda Creature: Creature.rating)
 
     for i in range(0, len(elite)):
-        if i == config.gen_elite_size:
+        if i is config.gen_elite_size:
             del elite[i:len(elite)]
             break
 
@@ -210,92 +212,102 @@ for epoch in range(config.gen_epochs):
         y = x + 1
         for _ in range(config.gen_num_inherited - x - 1):
             print(x, " + ", y)
-            if (config.stability_verification):
+            if config.stability_verification:
                 print("Veryfing parent stability")
-                if (validate_stability(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[y]].Ki)):
-                    new_generation.append(Creature(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[y]].Ki))
+                if (validate_stability(population[sorted_scores_indexes[x]].k_p,
+                                       population[sorted_scores_indexes[y]].k_i)):
+                    new_generation.append(Creature(population[sorted_scores_indexes[x]].k_p,
+                                                   population[sorted_scores_indexes[y]].k_i))
                 else:
-                    Kp, Ki = redefine_kp_ki_to_stable(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[y]].Ki)
-                    new_generation.append(Creature(Kp, Ki))
-                if (validate_stability(population[sorted_scores_indexes[y]].Kp, population[sorted_scores_indexes[x]].Ki)):
-                    new_generation.append(Creature(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[y]].Ki))
+                    k_p, k_i = redefine_kp_ki_to_stable(population[sorted_scores_indexes[x]].k_p,
+                                                      population[sorted_scores_indexes[y]].k_i)
+                    new_generation.append(Creature(k_p, k_i))
+                if (validate_stability(population[sorted_scores_indexes[y]].k_p,
+                                       population[sorted_scores_indexes[x]].k_i)):
+                    new_generation.append(Creature(population[sorted_scores_indexes[x]].k_p,
+                                                   population[sorted_scores_indexes[y]].k_i))
                 else:
-                    Kp, Ki = redefine_kp_ki_to_stable(population[sorted_scores_indexes[y]].Kp, population[sorted_scores_indexes[x]].Ki)
-                    new_generation.append(Creature(Kp, Ki))
+                    k_p, k_i = redefine_kp_ki_to_stable(population[sorted_scores_indexes[y]].k_p,
+                                                      population[sorted_scores_indexes[x]].k_i)
+                    new_generation.append(Creature(k_p, k_i))
             else:
-                new_generation.append(Creature(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[y]].Ki))
-                new_generation.append(Creature(population[sorted_scores_indexes[y]].Kp, population[sorted_scores_indexes[x]].Ki))
+                new_generation.append(Creature(population[sorted_scores_indexes[x]].k_p,
+                                               population[sorted_scores_indexes[y]].k_i))
+                new_generation.append(Creature(population[sorted_scores_indexes[y]].k_p,
+                                               population[sorted_scores_indexes[x]].k_i))
             y = y + 1
         x = x + 1
     print("New generation creation - crossed creatures added!")
     if config.debug_level != 1:
-        iter = 0
+        cntr = 0
         for creature in new_generation:
-            print("New generation creature ", iter)
-            print(creature.Kp)
-            print(creature.Ki)
-            iter = iter + 1
+            print("New generation creature ", cntr)
+            print(creature.k_p)
+            print(creature.k_i)
+            cntr = cntr + 1
 
     new_generation_size = len(new_generation)
 
     #Replicating parents
     print("Replicating parents...")
     for x in range(config.gen_num_replicated):
-        new_generation.append(Creature(population[sorted_scores_indexes[x]].Kp, population[sorted_scores_indexes[x]].Ki))
+        new_generation.append(Creature(population[sorted_scores_indexes[x]].k_p,
+                                       population[sorted_scores_indexes[x]].k_i))
     print("New generation creation - replicated creatures added!")
     if config.debug_level != 1:
-        iter2 = 0
+        cntr2 = 0
         for creature in new_generation:
-            if iter2 < new_generation_size:
-                iter2 = iter2 + 1
+            if cntr2 < new_generation_size:
+                cntr2 = cntr2 + 1
                 continue
-            print("New generation creature ", iter2)
-            print(creature.Kp)
-            print(creature.Ki)
-            iter2 = iter2 + 1
+            print("New generation creature ", cntr2)
+            print(creature.k_p)
+            print(creature.k_i)
+            cntr2 = cntr2 + 1
     new_generation_size = len(new_generation)
 
     #Adding randoms
     print("Adding new random parents")
     for _ in range(config.gen_num_random):
-        if (config.stability_verification == True):
-            Kp,Ki = draw_stable_kp_ki()
-            new_generation.append(Creature(Kp,Ki))
+        if config.stability_verification is True:
+            k_p,k_i = draw_stable_kp_ki()
+            new_generation.append(Creature(k_p,k_i))
         else:
-            new_generation.append(Creature(random.uniform(0, config.gen_max_kp), random.uniform(0, config.gen_max_ki))) #nosec
+            new_generation.append(Creature(random.uniform(0, config.gen_max_kp),
+                                           random.uniform(0, config.gen_max_ki))) #nosec
 
     print("New generation creation - random creatures added!")
     if config.debug_level != 1:
-        iter2 = 0
+        cntr2 = 0
         for creature in new_generation:
-            if iter2 < new_generation_size:
-                iter2 = iter2 + 1
+            if cntr2 < new_generation_size:
+                cntr2 = cntr2 + 1
                 continue
-            print("New generation creature ", iter2)
-            print(creature.Kp)
-            print(creature.Ki)
-            iter2 = iter2 + 1
+            print("New generation creature ", cntr2)
+            print(creature.k_p)
+            print(creature.k_i)
+            cntr2 = cntr2 + 1
 
     #Mutation
     print("Mutants are coming...")
     for creature in new_generation:
         rand_x = random.uniform(-1, 1) #nosec
-        new_kp = creature.Kp + (rand_x * config.gen_mutation_coef)
+        new_kp = creature.k_p + (rand_x * config.gen_mutation_coef)
         new_kp = max(0, min(new_kp, config.gen_max_kp))
         rand_y = random.uniform(-1, 1) #nosec
-        new_ki = creature.Ki + (rand_y * config.gen_mutation_coef)
+        new_ki = creature.k_i + (rand_y * config.gen_mutation_coef)
         new_ki = max(0, min(new_ki, config.gen_max_ki))
         if not validate_stability(new_kp, new_ki):
             new_kp, new_ki = redefine_kp_ki_to_stable(new_kp, new_ki)
         creature.mutate(new_kp, new_ki)
     print("Mutation finished!")
     if config.debug_level != 1:
-        iter = 0
+        cntr = 0
         for creature in new_generation:
-            print("New generation creature ", iter)
-            print(creature.Kp)
-            print(creature.Ki)
-            iter = iter + 1
+            print("New generation creature ", cntr)
+            print(creature.k_p)
+            print(creature.k_i)
+            cntr = cntr + 1
 
     #Print information about progress
     number_of_creatures = len(new_generation)
@@ -309,18 +321,18 @@ for epoch in range(config.gen_epochs):
     population = new_generation
 
 
-with open("ptp_optimization.log", "a") as f:
-    f.write(f"\n***************************************************************\n")
+with open("ptp_optimization.log", "a", encoding="utf-8") as f:
+    f.write("\n***************************************************************\n")
     f.write("Genetic algorithm best results:\n")
     os.chmod("ptp_optimization.log", 0o600)
     for creature in elite:
-        f.write(f"Kp: {creature.Kp}, Ki: {creature.Ki}, Score: {creature.rating}\n")
+        f.write(f"k_p: {creature.k_p}, k_i: {creature.k_i}, Score: {creature.rating}\n")
 
 default = Creature(0.7,0.3)
 default.evaluate_data(args.i, args.t)
 
-with open("ptp_optimization.log", "a") as f:
-    f.write(f"\n***************************************************************\n")
+with open("ptp_optimization.log", "a", encoding="utf-8") as f:
+    f.write("\n***************************************************************\n")
     f.write("Default settings results:\n")
-    f.write(f"Kp: {default.Kp}, Ki: {default.Ki}, Score: {default.rating}\n")
+    f.write(f"k_p: {default.k_p}, k_i: {default.k_i}, Score: {default.rating}\n")
     os.chmod("ptp_optimization.log", 0o600)
